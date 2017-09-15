2e976675e176ecbc5395728cc4a834d62ad3aa0f
Aimeelr <aimeelynnramirez@gmail.com>
2017-09-14 22:30:50 -0400

    AR - Images * scripts/products/populate-products.js -populated the products -imageLink url.jpeg -price 1 through 12 for each fruit vs veggie = products -title edited to singular item -first is a lizard image...(just because enjoy)

--- scripts/products/populate-products.js
+++ scripts/products/populate-products.js
@@ -1,15 +1,134 @@
 // insert Document
 db.products.insertMany(
-  [
-    {
-      title: 'lizard',
-      price: 23,
-      imageLink: 'https://i.imgur.com/MaBYUjZ.jpg'
-    },
-    {
-      title: 'kiwi',
-      price: 900,
-      imageLink: 'https://i.imgur.com/MaBYUjZ.jpg'
-    }
+  [{
+    title: 'lizard',
+    price: 23,
+    imageLink: 'https://i.imgur.com/qIwuJ2A.jpg'
+  },
+  {
+    title: 'kiwi',
+    price: 1,
+    imageLink: 'https://i.imgur.com/p1GyaV9.jpg'
+  },
+  {
+    title: 'elderberry',
+    price: 2,
+    imageLink: 'https://i.imgur.com/klHerK7.jpg'
+  },
+  {
+    title: 'papaya',
+    price: 3,
+    imageLink: 'https://i.imgur.com/gYUVIBN.jpg'
+  },
+  {
+    title: 'pomegranate',
+    price: 4,
+    imageLink: 'https://i.imgur.com/9LHRLOA.jpg'
+  },
+  {
+    title: 'watermelon',
+    price: 5,
+    imageLink: 'https://i.imgur.com/v2xjqGO.jpg'
+  },
+  {
+    title: 'apple',
+    price: 6,
+    imageLink: 'https://i.imgur.com/jnCJL9l.jpg'
+  },
+  {
+    title: 'peach',
+    price: 7,
+    imageLink: 'https://i.imgur.com/XWywUHL.jpg'
+  },
+  {
+    title: 'banana',
+    price: 8,
+    imageLink: 'https://i.imgur.com/iGkke3p.jpg'
+  },
+  {
+    title: 'pineapple',
+    price: 9,
+    imageLink: 'https://i.imgur.com/sXBEQKx.jpg'
+  },
+  {
+    title: 'strawberry',
+    price: 10,
+    imageLink: 'https://i.imgur.com/S8zCxmi.jpg'
+  },
+  {
+    title: 'grape',
+    price: 11,
+    imageLink: 'https://i.imgur.com/f0B6O9y.jpg'
+  },
+  {
+    title: 'pear',
+    price: 12,
+    imageLink: 'https://i.imgur.com/IPYklYx.jpg'
+  },
+  {
+    title: 'broccoli',
+    price: 1,
+    imageLink: 'https://i.imgur.com/vQvB9zw.jpg'
+  },
+  {
+    title: 'mushroom',
+    price: 2,
+    imageLink: 'https://i.imgur.com/0Yd42au.jpg'
+  },
+  {
+    title: 'celery',
+    price: 2,
+    imageLink: 'https://i.imgur.com/0qip3mk.jpg'
+  },
+  {
+    title: 'avocado',
+    price: 3,
+    imageLink: 'https://i.imgur.com/ucVxRCD.jpg'
+  },
+  {
+    title: 'icebergLettuce',
+    price: 4,
+    imageLink: 'https://i.imgur.com/XMCbENG.jpg'
+  },
+  {
+    title: 'zucchini',
+    price: 5,
+    imageLink: 'https://i.imgur.com/EW7Wymg.jpg'
+  },
+  {
+    title: 'sweetPotato',
+    price: 6,
+    imageLink: 'https://i.imgur.com/iMnLukL.jpg'
+  },
+  {
+    title: 'cucumber',
+    price: 7,
+    imageLink: 'https://i.imgur.com/dTMWk6U.jpg'
+  },
+  {
+    title: 'okra',
+    price: 8,
+    imageLink: 'https://i.imgur.com/8YmTxbB.jpg'
+  },
+  {
+    title: 'beetRoot',
+    price: 9,
+    imageLink: 'https://i.imgur.com/GcwQ8ew.jpg'
+  },
+  {
+    title: 'asparagus',
+    price: 10,
+    imageLink: 'https://i.imgur.com/NSqv6uA.jpg'
+  },
+  {
+    title: 'redCabbage',
+    price: 11,
+    imageLink: 'https://i.imgur.com/9R6IAMz.jpg'
+  },
+  {
+    title: 'greenBean',
+    price: 12,
+    imageLink: 'https://i.imgur.com/npujlFu.jpg'
+  }
   ]
 )
