2ecf0db820deae2c6463aab29194542b46ca2f47
cmigz <cmignacca@gmail.com>
2017-09-16 10:18:28 -0400

    CM - MODIFIED: User Controller / Add-Product Curl
    ** app/controllers/users.js:
    - Defined the product model
    - Modified Function: addProduct
      - Logged req body to see info being obtained
      - Pushing the product info set over to a user's cart
      - resolved with a 200
    ** scripts/auth/add-product.sh:
    - Modified script to send right data, after seeing what was sent over
      req
    - Modified product data sent to TITLE rather then NAME to follow
      products model

--- scripts/auth/add-product.sh
+++ scripts/auth/add-product.sh
@@ -4,7 +4,7 @@ API="http://localhost:4741"
 URL_PATH="/users"
 ID="59bc228cfe20142b7adbdaf6"
 TOKEN="rXwB9gGngSB5yoxgWmtDcU4Tk9ZX2K3+8E5Sxi3e7ow=--WEe341ZKF/lnR0w1EGqmBXQDKTsvMQwLDSbDvT8ZPV4="
-NAME="banana"
+TITLE="banana"
 PRICE=2
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -12,11 +12,9 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
-    "user": {
-      "cart":{
-        "name": "'"${NAME}"'",
-        "price": "'"${PRICE}"'"
-      }
+    "products": {
+      "title": "'"${TITLE}"'",
+      "price": "'"${PRICE}"'"
     }
   }'
 
